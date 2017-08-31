1f558733ebdc79d897441a646bb8c5bcd87a8ca9
Mahdia Hashimy <mahdia@Mahdias-MBP.fios-router.home>
2017-08-30 23:21:06 -0400

    friendsjs

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Mahdia",
+	"photo" : "",
+	"scores" : [5,1,1,4,5,2,2,1,4,1]
+}, {
+	"name" : "Andrea",
+	"photo" : "",
+	"scores" : [4,3,2,2,1,2,4,4,5,1]
+}, {
+	"name" : "Steven",
+	"photo" : "",
+	"scores" : [2,2,3,1,4,2,2,2,4,1]
+}];
+
+
+module.exports = friends;
